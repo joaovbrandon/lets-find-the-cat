f36ee2152631f28e272aa8e0ce890358b19ecc9d
joaaoeu <hello@joaao.eu>
2019-08-11 02:00:43 +0100

    Updates

---
+++ precache-manifest.1d508cfe24796d5ae3e3a4ad42b2b948.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da19e4144eff6ee8f7816e9a41ef807",
+    "url": "/lets-find-the-cat/index.html"
+  },
+  {
+    "revision": "975b4a7db460a9a5945c",
+    "url": "/lets-find-the-cat/static/js/2.2472d7d5.chunk.js"
+  },
+  {
+    "revision": "192e4ae4d43812cdb102",
+    "url": "/lets-find-the-cat/static/js/main.3a8623b7.chunk.js"
+  },
+  {
+    "revision": "6857cf680e453cd1fb41",
+    "url": "/lets-find-the-cat/static/js/runtime~main.3249bd83.js"
+  }
+]);
